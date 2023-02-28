fa2b54701d64e979b2e4a3b37b794d6dd669712d
shelby <shelbynarnold@yahoo.com>
2023-02-27 18:26:22 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const names = ["Charlie", "Phil", "Jake"]
+
+
+function writeCards(names, eventName){
+    let messageArray = [""]
+    for (let i = 0; i < names.length; i++ ){
+     messageArray[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`
+    } return messageArray
+} 
+
+function countDown(num){
+    for (num = 10; num >= 0; num--){
+        console.log(num)
+    } 
+}
